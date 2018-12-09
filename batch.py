3164ce9de83d27a0aad9620a3a3fb15af1350259
Haoyang Sang <HSANG3@emory.edu>
2018-12-09 12:56:23 -0500

    update
    batch.py is working now

--- batch.py
+++ batch.py
@@ -3,7 +3,7 @@
 import os
 import subprocess
 
-path = r'C:\Users\HSANG3\Desktop\Raplyzer\lyricsGenerator'
+path = r"C:\Users\HSANG3\Desktop\Rhymer\lyricsGenerator"
 
 def IPA(dir=path, artist=None, album=None, print_stats=False, language='en-us', lookback=15):
 
@@ -12,16 +12,16 @@ def IPA(dir=path, artist=None, album=None, print_stats=False, language='en-us', 
     else:
         artists = os.listdir(dir)
     for artist in artists:
-        lyrics_dir = dir+'\\'+artist
+        artist_dir = dir+"\\"+artist
         print ("Analyzing artist: %s" % artist)
-        songs = os.listdir(lyrics_dir)
+        songs = os.listdir(artist_dir)
             # Only the .txt files
         songs = [song for song in songs if len(song)>=4 and song[-4:]=='.txt']
-        os.chdir(lyrics_dir)
         for song in songs:
+            song_dir = artist_dir+"\\"+song
             # create .ipa file
-            cmd = u"espeak -xqf %s --phonout= %s.ipa" %(song, song)
-            subprocess.run(cmd, stdout=subprocess.PIPE)
+            cmd = u"espeak -xqf %s --phonout=%s.ipa" %(song_dir, song_dir)
+            subprocess.run(cmd,shell=True)
             
 def main():
     IPA(dir=path, language='en-us', lookback=15)
